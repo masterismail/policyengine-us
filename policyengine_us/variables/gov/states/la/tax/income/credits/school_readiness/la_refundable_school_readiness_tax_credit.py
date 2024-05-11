d43ae9ff8bd19776847e86e95421b2d7d0ed3fff
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-05-11 21:10:22 +0530

    created different Louisiana refundable and non-refundable school readiness tax credit variables

--- policyengine_us/variables/gov/states/la/tax/income/credits/school_readiness/la_refundable_school_readiness_tax_credit.py
+++ policyengine_us/variables/gov/states/la/tax/income/credits/school_readiness/la_refundable_school_readiness_tax_credit.py
@@ -1,7 +1,6 @@
 from policyengine_us.model_api import *
 
-
-class la_school_readiness_credit(Variable):
+class la_refundable_school_readiness_tax_credit(Variable):
     value_type = float
     entity = TaxUnit
     label = "Louisiana school readiness tax credit"
@@ -12,21 +11,12 @@ class la_school_readiness_credit(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        p = parameters(
-            period
-        ).gov.states.la.tax.income.credits.school_readiness
+        p = parameters(period).gov.states.la.tax.income.credits.school_readiness
         # determine school readiness credit amount
-        child_care_credit = add(
-            tax_unit, period, ["la_refundable_cdcc", "la_non_refundable_cdcc"]
-        )
-        eligible_child = person(
-            "la_school_readiness_credit_eligible_child", period
-        )
-        quality_rating = person(
-            "la_quality_rating_of_child_care_facility", period
-        )
+        child_care_credit = add(tax_unit, period, ["la_refundable_cdcc"])
+        eligible_child = person("la_school_readiness_credit_eligible_child", period)
+        quality_rating = person("la_quality_rating_of_child_care_facility", period)
         child_credit_percent = child_care_credit * p.rate.calc(quality_rating)
         # la_cdcc times the percentage
         amount = eligible_child * child_credit_percent
-
         return tax_unit.sum(amount)
